735aefe535f61f42ced34c30a97ba2702afc56ea
Puneet-iner <imhtcy312@gmail.com>
2022-12-02 22:09:49 +0530

    Resolved bug

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 const app = require('./app');
-
 app.listen(4000, () => {
-    console.log(`Server listening on port 4000`);
+    console.log(`Server listening on port ${process.env.PORT || 4000}`);
 });
